6a99287d72da2877d289780a72e2cc9208bcfb11
rabuu <rabuu.dev@gmail.com>
2022-11-30 21:10:00 +0100

    Add `AddrMode::addr_size()` and `AddrOperand`

--- src/cpu/instruction/addressing.rs
+++ src/cpu/instruction/addressing.rs
@@ -26,3 +26,30 @@ pub enum AddrMode {
     /// immediate
     Imm,
 }
+
+impl AddrMode {
+    const fn addr_size(&self) -> u8 {
+        match self {
+            AddrMode::Imp => 0,
+            AddrMode::Abs => 2,
+            AddrMode::Abx => 2,
+            AddrMode::Aby => 2,
+            AddrMode::Zpg => 1,
+            AddrMode::Zpx => 1,
+            AddrMode::Zpy => 1,
+            AddrMode::Rel => 1,
+            AddrMode::Akk => 0,
+            AddrMode::Ind => 2,
+            AddrMode::Inx => 1,
+            AddrMode::Iny => 1,
+            AddrMode::Imm => 1,
+        }
+    }
+}
+
+pub enum AddrOperand {
+    Implied,
+    Immediate(u8),
+    Relative(u16),
+    MemAddr(u16),
+}
